35604d540f7baacf7d185f56b3d3704e7b91a73b
Dusty Dragon <darkpirate47171@gmail.com>
2024-12-06 21:29:51 +0530

    more Q added

---
+++ Arrays/src/basic/Make_Array_strictly_increasing.java
@@ -0,0 +1,7 @@
+package basic;
+
+public class Make_Array_strictly_increasing {
+    public static void main(String[] args) {
+        int m = Integer.MIN_VALUE;
+    }
+}
---
+++ Arrays/src/basic/Stocks_easy.java
@@ -0,0 +1,21 @@
+package basic;
+
+import com.sun.source.tree.BinaryTree;
+
+import java.util.Arrays;
+
+public class Stocks_easy {
+
+
+    public static int maxProfit(int[] prices) {
+
+
+
+        }
+
+
+    public static void main(String[] args) {
+       int ans =  maxProfit(new int[]{2,4,1});
+        System.out.println(ans);
+    }
+}
