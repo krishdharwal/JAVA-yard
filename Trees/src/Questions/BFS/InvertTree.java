f19b21af87f4b8452ca999de08fc51c1754d0c2d
krishdharwal <darkpirate47171@gmail.com>
2025-01-17 11:07:21 +0530

    invert binary tree question added

---
+++ Trees/src/Questions/BFS/InvertTree.java
@@ -0,0 +1,35 @@
+package Questions.BFS;
+
+import Questions.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class InvertTree {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null){
+            return root;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while(!queue.isEmpty()){
+
+            int level = queue.size();
+            for(int i=0; i<level; i++){
+                TreeNode node = queue.poll();
+
+                // swap left and right
+                TreeNode temp = node.left;
+                node.left = node.right;
+                node.right = temp;
+
+                if(node.left != null) queue.add(node.left);
+                if(node.right != null) queue.add(node.right);
+            }
+        }
+
+        return root;
+    }
+}
--- Trees/src/Questions/TreeNode.java
+++ Trees/src/Questions/TreeNode.java
@@ -2,9 +2,9 @@ package Questions;
 
 public class TreeNode {
     int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode next;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode next;
 
     TreeNode(){}
     TreeNode(int val){
