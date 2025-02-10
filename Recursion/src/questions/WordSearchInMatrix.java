791db7c80c0ebe9e65d9247e31a30f7639328341
krishdharwal <darkpirate47171@gmail.com>
2025-02-10 10:16:38 +0530

    word search optimised

--- Recursion/src/questions/WordSearchInMatrix.java
+++ Recursion/src/questions/WordSearchInMatrix.java
@@ -2,49 +2,47 @@ package questions;
 
 public class WordSearchInMatrix {
 
-    public boolean exist(char[][] board, String word) {
+    public static boolean exist(char[][] board, String word) {
         for (int r = 0; r < board.length; r++) {
             for (int c = 0; c < board[0].length; c++) {
+                if(board[r][c] == word.charAt(0) && word.length() == 1){
+                    return true;
+                }
                 if(board[r][c] == word.charAt(0)
-                        && findWord(board,new boolean[board.length][board[0].length],r,c,word.substring(1))){
+                        && findWord(board,new boolean[board.length][board[0].length],r,c,word)){
                     return true;
                 }
             }
         }
-        return istrue;
+        return false;
     }
 
-    boolean istrue = false;
-    public boolean findWord(char[][] board, boolean[][] bool, int r, int c, String word){
+    public static boolean  findWord(char[][] board, boolean[][] bool, int r, int c, String word){
         if (word.isEmpty()){
-            istrue = true;
             return true;
         }
-        if(!bool[r][c]){
+        if(bool[r][c] || board[r][c] != word.charAt(0)){
             return false;
         }
-        bool[r][c] = false;
-        char ch = word.charAt(0);
-        // checks
-        // down
-        if (r < board.length-1 && board[r+1][c] == ch){
-            if(findWord(board,bool,r+1,c,word.substring(1))) return true;
-        }
-
-        // right
-        if (c < board[0].length-1 && board[r][c+1] == ch){
-            if(findWord(board,bool,r,c+1,word.substring(1))) return true;
-        }
-
-        //  left
-        if (c > 0 && board[r][c-1] == ch){
-            if(findWord(board,bool,r,c-1,word.substring(1))) return true;
-        }
-        // up
-        if (r > 0 && board[r-1][c] == ch){
-            if( findWord(board,bool,r-1,c,word.substring(1))) return true;
-        }
         bool[r][c] = true;
-        return false;
+        char ch = word.charAt(0);
+        String newSubString = word.substring(1);
+
+         boolean found = ((r < board.length-1 && findWord(board,bool,r+1,c,word.substring(1))) ||
+          (c < board[0].length-1 && findWord(board,bool,r,c+1,word.substring(1))) ||
+          (c > 0 && findWord(board,bool,r,c-1,word.substring(1))) ||
+          (r > 0 && findWord(board,bool,r-1,c,word.substring(1))));
+
+        bool[r][c] = false;
+        return found;
+    }
+
+    public static void main(String[] args) {
+        char[][] board = {
+                {'A'},
+//                {'S', 'F', 'C', 'S'},
+//                {'A', 'D', 'E', 'E'}
+        };
+        System.out.println(exist(board,"ABCCED"));
     }
 }
