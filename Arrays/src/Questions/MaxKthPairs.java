3d2a372c155e044f5a35772d3dd7a2ee34b96051
krishdharwal <darkpirate47171@gmail.com>
2025-03-22 18:36:00 +0530

    Max Kth pairs added

---
+++ Arrays/src/Questions/MaxKthPairs.java
@@ -0,0 +1,37 @@
+package Questions;
+
+import java.util.Arrays;
+
+public class MaxKthPairs {
+
+    public int maxOperations(int[] nums, int k) {
+        Arrays.sort(nums);
+        int i = 0;
+        int j = nums.length -1;
+
+        int pairs = 0;
+        while (i < j){
+            int sum = nums[j] + nums[i]
+            if (sum == k){
+                pairs++;
+                j--;
+                i++;
+            }
+            else if (sum > k){
+                j--;
+            }else {
+                i++;
+            }
+
+        }
+
+        return pairs;
+    }
+
+    public static void main(String[] args) {
+        MaxKthPairs m = new MaxKthPairs();
+       int ans =  m.maxOperations(new int[]{4,4,1,3,1,3,2,2,5,5,1,5,2,1,2,3,5,4},2);
+       System.out.println(ans);
+    }
+
+}
--- Arrays/src/Questions/Triplet.java
+++ Arrays/src/Questions/Triplet.java
@@ -1,12 +1,12 @@
-    public boolean increasingTriplet(int[] nums) {
-      int i = Integer.MAX_VALUE;
-      int j = Integer.MAX_VALUE;
-
-      for(int ele : nums){
-        if(ele <= i) i = ele;
-        else if(ele <= j)  j = ele;
-        else return true;
-      }
-
-      return false;
-}
+//    public boolean increasingTriplet(int[] nums) {
+//      int i = Integer.MAX_VALUE;
+//      int j = Integer.MAX_VALUE;
+//
+//      for(int ele : nums){
+//        if(ele <= i) i = ele;
+//        else if(ele <= j)  j = ele;
+//        else return true;
+//      }
+//
+//      return false;
+//}
