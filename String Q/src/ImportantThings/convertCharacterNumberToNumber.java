378fa6b09baf6e7f0bbce82fa068b0b98e00b898
krishdharwal <darkpirate47171@gmail.com>
2025-05-01 07:54:58 +0530

    if to add previously done number , anything you want to get add with it

--- String Q/src/ImportantThings/convertCharacterNumberToNumber.java
+++ String Q/src/ImportantThings/convertCharacterNumberToNumber.java
@@ -2,10 +2,16 @@ package ImportantThings;
 
 public class convertCharacterNumberToNumber {
     public static void main(String[] args) {
-        // actual conversion
+        // 1.
         char chNum = '3';
         int afterConversion = chNum - '0';
         System.out.println(afterConversion);
 
+        // 2.
+        // if to add previously done number , anything you want to get add with it
+        char c = '4';
+        int n = 3;
+         n = n * 10 + (c - '0');
+        System.out.println(n); //34
     }
 }
