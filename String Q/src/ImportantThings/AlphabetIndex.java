37640015815153044fe8b80474301cb950df8ee3
krishdharwal <darkpirate47171@gmail.com>
2025-05-01 07:47:48 +0530

    important things floder added in Strings > Content - convert character number to number and to find any alpha bet index

---
+++ String Q/src/ImportantThings/AlphabetIndex.java
@@ -0,0 +1,13 @@
+package ImportantThings;
+
+public class AlphabetIndex {
+    public static void main(String[] args) {
+
+        // to get the alphabet index
+        // eg -> a = 1 , b = 2 , c  = 3
+        char ch2 = 'g';
+        int num2 = ch2 - 'a' + 1;
+        System.out.println(num2);
+
+    }
+}
---
+++ String Q/src/ImportantThings/convertCharacterNumberToNumber.java
@@ -0,0 +1,11 @@
+package ImportantThings;
+
+public class convertCharacterNumberToNumber {
+    public static void main(String[] args) {
+        // actual conversion
+        char chNum = '3';
+        int afterConversion = chNum - '0';
+        System.out.println(afterConversion);
+
+    }
+}
