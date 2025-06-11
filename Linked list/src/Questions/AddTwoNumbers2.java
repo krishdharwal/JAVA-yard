f6bfc06b319c2bf2347334b23dde6ab6aa32a93b
krishdharwal <darkpirate47171@gmail.com>
2025-06-11 18:45:58 +0530

    add two numbers PART - 2 , in this we reverse the ll and return the answer with reversed ll answer

--- Linked list/src/Questions/AddTwoNumbers2.java
+++ Linked list/src/Questions/AddTwoNumbers2.java
@@ -1,15 +1,15 @@
 package Questions;
 import Questions.Linkedlist.ListNode;
 
-import java.util.List;
-import java.util.Stack;
 
-class AddTwoNumbers {
+class AddTwoNumbers2 {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        l1 = reverse(l1);
+        l2 = reverse(l2);
+
         ListNode head = new ListNode();
         ListNode tail = head;
         int carry = 0;
-
         while (l1 != null || l2 != null || carry != 0){
             int n1 = (l1 != null) ? l1.val : 0;
             int n2 = (l2 != null) ? l2.val : 0;
@@ -17,14 +17,25 @@ class AddTwoNumbers {
             int sum = n1 + n2 + carry;
             int rem = sum % 10;
             carry = sum / 10;
+
             tail.next = new ListNode(rem);
             tail = tail.next;
 
-            l1 = (l1 != null) ? l1.next : null;
-            l2 = (l2 != null) ? l2.next : null;
+            l1 = (l1 != null) ? l1.next : l1;
+            l2 = (l2 != null) ? l2.next : l2;
         }
-
-        return head.next;
+        return reverse(head.next);
     }
 
+    public ListNode reverse(ListNode curr){
+        ListNode prev = null;
+        ListNode fwd = curr.next;
+        while (curr != null){
+            curr.next = prev;
+            prev = curr;
+            curr = fwd;
+            if (fwd != null) fwd = fwd.next;
+        }
+        return prev;
+    }
 }
