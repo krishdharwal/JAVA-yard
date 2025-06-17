2c0a5e503098d767c4a5574998c6f033f865e0d4
krishdharwal <darkpirate47171@gmail.com>
2025-06-17 20:06:27 +0530

    valid anagram added

--- String Q/src/Questions/RepeatedSubstringPattern.java
+++ String Q/src/Questions/RepeatedSubstringPattern.java
@@ -1,7 +1,6 @@
 package Questions;
 
 class  RepeatedSubstringPattern {
-
     public boolean repeatedSubstringPattern(String s) {
         int n = s.length();
         for (int i = 1; i <= n/2; i++) {
--- String Q/src/Questions/ValidAnagram.java
+++ String Q/src/Questions/ValidAnagram.java
@@ -4,19 +4,11 @@ import java.util.Arrays;
 
 class ValidAnagram {
     public boolean isAnagram(String s, String t) {
-        return false;
-    }
-
-    public static void main(String[] args) {
-        String s = "anagram";
-        String t = "nagaram";
         char[] arr = s.toCharArray();
         char[] arr2 = t.toCharArray();
         Arrays.sort(arr);
         Arrays.sort(arr2);
-        System.out.println(Arrays.toString(arr));
-        System.out.println(Arrays.toString(arr2));
-        System.out.println(Arrays.equals(arr,arr2));
-
+        return Arrays.equals(arr,arr2);
     }
+
 }
