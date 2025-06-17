263b25e417cb9069018f639f4aa6ab524ecb0bd8
krishdharwal <darkpirate47171@gmail.com>
2025-06-17 20:01:01 +0530

    Repeated Substring Pattern added

---
+++ String Q/src/Questions/RepeatedSubstringPattern.java
@@ -0,0 +1,25 @@
+package Questions;
+
+class  RepeatedSubstringPattern {
+
+    public boolean repeatedSubstringPattern(String s) {
+        int n = s.length();
+        for (int i = 1; i <= n/2; i++) {
+            if (n % i == 0){
+                System.out.println(n%i);
+                // call to find the repetition
+                if (haveRepeated(s,s.substring(0,i))) return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean haveRepeated(String s, String t) {
+        int n = t.length();
+        for (int i = 0; i < s.length(); i = i + n) {
+            if (!s.substring(i,i+n).equals(t)) return false;
+        }
+        return true;
+    }
+
+}
